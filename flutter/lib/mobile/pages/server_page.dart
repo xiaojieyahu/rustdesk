fa1600c24b38eecdee97e61b8db66e26d527b46d
xiaojieyahu <501117733@qq.com>
2024-08-08 09:55:42 +0800

    更新设置5

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -585,7 +585,7 @@ class _PermissionCheckerState extends State<PermissionChecker> {
               //         gFFI.userModel.userName.value.isEmpty &&
               //         bind.mainGetLocalOption(key: "show-scam-warning") == "N"
               //     ? () => showScamWarning(context, serverModel)
-              //     : serverModel.toggleService),
+                 serverModel.toggleService),
           PermissionRow(translate("Input Control"), serverModel.inputOk,
               serverModel.toggleInput),
           PermissionRow(translate("Transfer file"), serverModel.fileOk,
